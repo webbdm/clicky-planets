22595890b584379c60dea95a2dbfd353794d0dd2
webbdm <gwebb762@gmail.com>
2017-02-21 19:45:58 -0600

    added for loop

--- planets.js
+++ planets.js
@@ -1,4 +1,61 @@
-body{
+var getPlanets = document.getElementById('showPlanets');
+var planetHolder = document.getElementById("planet-holder");
 
-	background-color: teal;
+
+var planets = [
+    {
+        name:"Mercury",
+        url: ""
+    },
+    {
+        name:"Venus",
+        url: ""
+    },
+    {
+        name:"Earth",
+        url: ""
+    },
+    {
+        name:"Mars",
+        url: ""
+    },
+    {
+        name:"Jupiter",
+        url: ""
+    },
+    {
+        name:"Saturn",
+        url: ""
+    },
+    {
+        name:"Uranus",
+        url: ""
+    },
+    {
+        name:"Neptune",
+        url: ""
+    }
+];
+
+
+
+var array = [];
+
+
+function writeToDom(){
+
+	for (var i = 0; i < planets.length; i++) {
+		
+		var newPlanet="";
+		newPlanet += `<div class="planetBox" id="planetBox-${i}">`
+		newPlanet += `<div class="planetName"> ${planets[i].name} </div>`;
+		newPlanet += `</div>`
+		planetHolder.innerHTML += newPlanet;
+	}
+
 }
+
+getPlanets.addEventListener('click', writeToDom)
+
+
+
